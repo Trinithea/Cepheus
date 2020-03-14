73f39f4e362b0bb927d2e12268ab5c37aead89b5
Trinity Mazna <mysakmazplus@gmail.com>
2020-03-14 18:20:42 +0100

    Different types of vertices

---
+++ Cepheus_OOP/GraphEntities/Graph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cepheus_OOP
+{
+	class Graph
+	{
+		public static Dictionary<string, Edge> Edges = new Dictionary<string, Edge>();
+		public static Dictionary<string, Vertex> Vertices = new Dictionary<string, Vertex>();
+
+	}
+}
