b6c3d96e64c0f2aff36eefec81956aba175bb964
Trinity Mazna <mysakmazplus@gmail.com>
2020-03-19 17:46:12 +0100

    Binary heap is unfinished, maybe will  be substitute by SortedList

---
+++ CepheusProject/DataStructures/BinaryHeap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CepheusProject.DataStructures
+{
+	class MinimumBinaryHeap
+	{
+		public List<Node> heap = new List<Node>();
+		public Node Root { get; set; }
+		int CountOFNodes = -1; // because firts node will be on index 0
+		public void Insert(Node newNode)
+		{
+			CountOFNodes++;
+
+		}
+	}
+	class Node
+	{
+		public Node LeftSon { get; set; }
+		public Node RightSon { get; set; }
+		public int Key { get; set; }
+	}
+}
