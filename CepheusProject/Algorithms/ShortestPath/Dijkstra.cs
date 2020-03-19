cff237361fb4234e18ee4fc16ac8f96f4715873d
Trinity Mazna <mysakmazplus@gmail.com>
2020-03-19 17:46:25 +0100

    Dijkstra started

---
+++ CepheusProject/Algorithms/ShortestPath/Dijkstra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cepheus
+{
+	class Dijkstra : IAlgorithm
+	{
+		public static string Name => "Dijkstra's algorithm";
+
+		public void Run(Graph<BfsVertex> graph, BfsVertex initalVertex)
+		{
+
+		}
+	}
+}
--- CepheusProject/GraphEntities/Edge.cs
+++ CepheusProject/GraphEntities/Edge.cs
@@ -13,4 +13,13 @@ namespace Cepheus
 		public string Name { get; set; }
 		
 	}
+	class EdgeWithNaturalLength<T> : Edge<T> where T : Vertex
+	{
+		public int Length { get => Length; set {
+				if (value <= 0)
+					throw new ArgumentOutOfRangeException(); // TODO something more user friendly
+				else
+					Length = value;
+			} }
+	}
 }
