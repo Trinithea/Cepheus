b9662eb645e7257dbdc4bf4f63cb75b0157f14c8
Trinity Mazna <mysakmazplus@gmail.com>
2020-03-19 14:58:38 +0100

    DFS algorithm implementation

---
+++ CepheusProject/Algorithms/Searching/DFS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cepheus
+{
+	class DFS : IAlgorithm
+	{
+		public string Name => "Depth-first search";
+		int Time = 0;
+		public void Run(Graph<DfsVertex> graph,DfsVertex initialVertex)
+		{
+			//Initialized vertices and StepCount by default
+			
+			Recursion(initialVertex);
+		}
+		void Recursion(DfsVertex vertex)
+		{
+			vertex.State = IStateVertex.States.Open;
+			Time++;
+			vertex.InTime = Time;
+		}
+	}
+}
