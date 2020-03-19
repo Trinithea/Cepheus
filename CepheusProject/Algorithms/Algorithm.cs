90313fc6e9252b5a97b868fdded07e751996e061
Trinity Mazna <mysakmazplus@gmail.com>
2020-03-19 11:54:22 +0100

    BFS implmentation added

---
+++ CepheusProject/Algorithms/Algorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cepheus
+{
+	public interface IAlgorithm
+	{
+		public string Name { get; }
+		
+	}
+}
---
+++ CepheusProject/Algorithms/Searching/BFS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cepheus
+{
+	class BFS : IAlgorithm
+	{
+		public string Name => "Breadth-first search";
+
+		public void Run (Graph graph, BfsVertex initialVertex)
+		{
+			// Initialization is done in BfsVertex constructor
+
+			initialVertex.State = StateVertex.States.Open;
+			initialVertex.Distance = 0;
+
+			Queue<BfsVertex> queue = new Queue<BfsVertex>();
+			queue.Enqueue(initialVertex);
+
+			while(queue.Count > 0)
+			{
+				BfsVertex vertex = queue.Dequeue();
+				foreach(Edge<BfsVertex> edge in vertex.OutEdges)
+				{
+					if (edge.To.State == StateVertex.States.Unvisited)
+					{
+						edge.To.State = StateVertex.States.Open;
+						edge.To.Distance = vertex.Distance + 1;
+						edge.To.Predecessor = vertex;
+						queue.Enqueue(edge.To);
+					}
+				}
+				vertex.State = StateVertex.States.Closed;
+			}
+
+		}
+	}
+}
